34c355e4848b8ce8be59625856728011626e6cd8
h-hakimov <57480412+h-hakimov@users.noreply.github.com>
2019-11-09 16:11:20 +0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,7 +37,7 @@ public class Main {
                     browser.screenshot();
                 }
             }
-            //browser.closeBrowser();
+            browser.closeBrowser();
         }
     }
     
